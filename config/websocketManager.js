050915ae8845081f10d6e55b306dda5155fdd7c6
Hemershow <hemersonviolin@gmail.com>
2024-05-23 14:17:08 -0300

    Trying to fix circular structure

--- config/websocketManager.js
+++ config/websocketManager.js
@@ -24,11 +24,24 @@ function sendMessageToAllClients(data) {
         return;
     }
 
+    const cache = new Set();
+    const dataString = JSON.stringify(data, function (key, value) {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.has(value)) {
+                return;
+            }
+            cache.add(value);
+        }
+        return value;
+    });
+
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify(data));
         }
     });
+
+    cache.clear();
 }
 
 module.exports = { initializeWebSocketServer, sendMessageToAllClients };
